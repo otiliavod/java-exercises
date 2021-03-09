7524621b4616245fb636d1817d7e3e3c615bee9a
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-09 21:17:09 +0200

    Updated file and content

---
+++ Lab1/QuickStart.java
@@ -0,0 +1,14 @@
+class QuickStart {
+    public static void main(String[] args) { // asta trebuie memorata asa cum e
+        System.out.println("Hello, World.");
+        int x = 3;
+        System.out.println(x);
+    }
+} 
+
+//paying attention? you dont really need to use the github desktop while having git as cmd installed
+
+//basically you can do it from here now, see the green dot and U? it means updated from what it is already online
+// see the icon with 2? it shows you have made 2 changes that arent online yet
+// D means deleted (when you rename a file it actually gets deleted and a new one is created)
+// initially the file was called P1, now it's quickstart
