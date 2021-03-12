89f556e0a91dc6fe883f370fa33157c6a7dda1e5
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-13 00:57:51 +0200

    Recursion Example

---
+++ TheNewBostonIntermediate/Recursion/Main.java
@@ -0,0 +1,16 @@
+package TheNewBostonIntermediate.Recursion;
+
+public class Main {
+    public static void main(String args[]) {
+        System.out.println(fact(5));
+    }
+
+    public static long fact(long n) {
+        if(n <= 1) {
+            return 1;
+        }
+        else {
+            return n * fact(n-1);
+        } 
+    }
+}
