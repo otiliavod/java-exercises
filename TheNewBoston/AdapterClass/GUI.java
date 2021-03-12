ce8ece4090147c9e501714cdd327e61615ef02ab
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-12 17:30:03 +0200

    Adapter Class

---
+++ TheNewBoston/AdapterClass/GUI.java
@@ -0,0 +1,39 @@
+package TheNewBoston.AdapterClass;
+import java.awt.*;
+import java.awt.event.*;
+import javax.swing.*;
+
+public class GUI extends JFrame{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    private String details;
+    private JLabel statusbar;
+
+    public GUI() {
+        super("Title");
+
+        statusbar = new JLabel("This is default");
+        add(statusbar, BorderLayout.SOUTH);
+        addMouseListener(new Mouseclass());
+    }
+
+    private class Mouseclass extends MouseAdapter {
+        public void mouseClicked(MouseEvent event) {
+            details = String.format("You clicked %d", event.getClickCount());
+
+            if(event.isMetaDown()) {
+                details += "with right mouse button";
+            }
+            else if(event.isAltDown()) {
+                details += "with center mouse button";
+            }
+            else {
+                details += "with left mouse button";
+            }
+            statusbar.setText(details);
+        }
+    }
+    
+}
