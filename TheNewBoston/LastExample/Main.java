f99f13c1937265435f0533c17b7ee0fbff7af993
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-12 19:36:52 +0200

    String Example

---
+++ TheNewBoston/LastExample/Main.java
@@ -0,0 +1,18 @@
+package TheNewBoston.LastExample;
+
+public class Main {
+    public static void main(String args[]) {
+        String a = "apples";
+        String b = "bucky";
+        String c = "BUCKY";
+        System.out.println(a.length());
+        
+        if(a.equals("apples")) {
+            System.out.println("It equals apples");
+        }
+
+        if(b.equalsIgnoreCase(c)) { 
+            System.out.println("They are equal");
+        }
+    }
+}
