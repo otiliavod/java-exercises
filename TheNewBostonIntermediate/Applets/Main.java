4d48a1a60d483679605a45e165ef750652d1ada1
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-14 20:44:27 +0200

    Update Main.java
    JApplet

--- TheNewBostonIntermediate/Applets/Main.java
+++ TheNewBostonIntermediate/Applets/Main.java
@@ -1,33 +1,32 @@
-package TheNewBostonIntermediate.Applets;
-import java.awt.*;
-import javax.swing.*;
+//package TheNewBostonIntermediate.Applets;
+//import java.awt.*;
+//import javax.swing.*;
 
-public class Main extends JApplet{
+//public class Main extends JApplet{
     /**
      *
      */
-    private static final long serialVersionUID = 1L;
-    public static void main(String[] args) {
+   // private static final long serialVersionUID = 1L;
+    //public static void main(String[] args) {
         //public void paint(Graphics g) {
            // super.paint(g);
            // g.drawString("Abc", 25, 25);
       //  }
         
-    } //Init:
-    private double sum ;
-        public void init() {
-            String fn = JOptionPane.showInputDialog("Enter firt number");
-            String sn = JOptionPane.showInputDialog("Enter second number");
+   // } //Init:
+   // private double sum ;
+       // public void init() {
+           // String fn = JOptionPane.showInputDialog("Enter firt number");
+           // String sn = JOptionPane.showInputDialog("Enter second number");
             
-            double n1 = Double.parseDouble(fn);
-            double n2 = Double.parseDouble(sn);
+           // double n1 = Double.parseDouble(fn);
+           // double n2 = Double.parseDouble(sn);
     
-            sum = n1 + n2;
-        }
+          //  sum = n1 + n2;
+       // }
 
-     public void paint(Graphics g) {
-         super.paint(g);
-        g.drawString("The sum is " + sum, 25, 25);
-     }
-    
-}
+    // public void paint(Graphics g) {
+       //  super.paint(g);
+       // g.drawString("The sum is " + sum, 25, 25);
+    // }   
+//}
