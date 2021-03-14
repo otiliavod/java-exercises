7c58a754d883d97b6b21ea73863665f7526a75ca
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-15 01:31:26 +0200

    Create Main.java
    Average Example

---
+++ JavaExercises/Lab1/P2/Main.java
@@ -0,0 +1,27 @@
+/* Se citesc de la tastatura N numere intregi. Sa se calculeze media
+ lor aritmetica.*/
+
+package JavaExercises.Lab1.P2;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        int N, a;
+        int suma = 0;
+        double medie;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("N = ");
+        N = sc.nextInt();
+
+        for(int i=0; i < N; i++) { // restul numerelor pana la N
+            System.out.print("a = ");
+            a = sc.nextInt();
+            suma = suma + a;
+        }
+
+        medie = suma / N;
+
+        System.out.println("Media este: " + medie);
+        sc.close();
+    }
+}
