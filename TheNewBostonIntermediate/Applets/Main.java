078d8110efb12e716ec52e315f88238da98be63c
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-14 19:44:58 +0200

    Create Main.java
    Applet Example

---
+++ TheNewBostonIntermediate/Applets/Main.java
@@ -0,0 +1,33 @@
+package TheNewBostonIntermediate.Applets;
+import java.awt.*;
+import javax.swing.*;
+
+public class Main extends JApplet{
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
+    public static void main(String[] args) {
+        //public void paint(Graphics g) {
+           // super.paint(g);
+           // g.drawString("Abc", 25, 25);
+      //  }
+        
+    } //Init:
+    private double sum ;
+        public void init() {
+            String fn = JOptionPane.showInputDialog("Enter firt number");
+            String sn = JOptionPane.showInputDialog("Enter second number");
+            
+            double n1 = Double.parseDouble(fn);
+            double n2 = Double.parseDouble(sn);
+    
+            sum = n1 + n2;
+        }
+
+     public void paint(Graphics g) {
+         super.paint(g);
+        g.drawString("The sum is " + sum, 25, 25);
+     }
+    
+}
