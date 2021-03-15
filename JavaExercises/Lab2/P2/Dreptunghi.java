3c23fdae46ae79cc3027ba4e768c34aa4f92f455
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-15 23:30:10 +0200

    Rectangle Addition

---
+++ JavaExercises/Lab2/P2/Dreptunghi.java
@@ -0,0 +1,15 @@
+/* Ce trebuie adaugat in clasa Dreptunghi astfel incat pentru un
+obiect Dreptunghi sa ii putem afisa lungimile laturilor?*/
+
+package JavaExercises.Lab2.P2;
+
+public class Dreptunghi {
+    private int a;
+    private int b;
+
+    // Completare la PS5 :
+    
+    public void afisare() {
+        System.out.println("Laturile: " + a + "," + b);
+    }
+}
