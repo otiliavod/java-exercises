7bba8d94d20778fdd572f728c0e2f7bb1231cebc
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-17 23:21:20 +0200

    Number Frequency Example

--- JavaExercises/Lab4/P4/Main.java
+++ JavaExercises/Lab4/P4/Main.java
@@ -35,7 +35,7 @@ public class Main {
             }
         }
 
-        System.out.println(val + " apare de " + max + " ori");
+        System.out.println(val + " apare de " + max + " ori ");
         sc.close();
     }
 }
