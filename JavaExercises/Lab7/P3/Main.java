c9d96328f69e9d27886348a4baf9efe6632aab6c
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-20 19:18:13 +0200

    Initial commit

---
+++ JavaExercises/Lab7/P3/Main.java
@@ -0,0 +1,8 @@
+/* Se citeste de la tastatura o propozitie. Sa se copieze toate cuvinte care incep cu vocala intr-un vectori 
+de siruri si sa se afiseze acel vector.*/
+
+package JavaExercises.Lab7.P3;
+
+public class Main {
+    
+}
