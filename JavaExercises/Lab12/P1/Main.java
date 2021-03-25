04e3eec22ff834a47bf248e7db193ded3d8cf53e
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-25 22:32:43 +0200

    Character File Example

--- JavaExercises/Lab12/P1/Main.java
+++ JavaExercises/Lab12/P1/Main.java
@@ -5,33 +5,35 @@ package JavaExercises.Lab12.P1;
 import java.io.*;
 
 public class Main {
-    public static void main(String[] args) {
-        FileReader fr = null;
+    public static void main(String[] args) throws IOException {
         BufferedReader bfr = null;
         FileWriter fw = null;
         BufferedWriter bfw = null;
 
         try {
-            fw = new FileWriter("linie.txt");
+            fw = new FileWriter("C:\\Users\\Otii\\Documents"+
+            "\\GitHub\\java-exercises\\JavaExercises\\Lab12\\P1\\linie.txt");
             bfw = new BufferedWriter(fw);
             bfr = new BufferedReader(new InputStreamReader(System.in));
             System.out.print("Sir: ");
             String s = bfr.readLine();
             bfw.write(s);
-            fr = new FileReader(s);
-
+            bfw.newLine();
             for(int i=0; i<s.length(); i++) {
                 char c = s.charAt(i);
                 bfw.write(c);
                 bfw.newLine();
             }
-            fr.close();
-            bfw.close();
-            bfr.close();
-            fw.close();
+           
         } catch(IOException e) {
             System.out.println(e);
             System.exit(1);
+        } 
+        finally{
+            bfw.close();
+            bfr.close();
+            fw.close();
         }
+       
     }
 }
