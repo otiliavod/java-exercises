11fbb829fd7c709cd3f37828fa5f8ee4ed4784a7
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-25 16:11:27 +0200

    Numbers File Example

---
+++ JavaExercises/Lab12/P3/Main.java
@@ -0,0 +1,40 @@
+/* Se citeste de la tastatura un numar natural N. Sa se creeze prin program un
+fisier text ce are 10 linii, astfel: in prima linie N valori de 0 separate
+intre ele printr-un spatiu, in a doua linie N valori de 1, etc.*/
+
+package JavaExercises.Lab12.P3;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) {
+        final int DIM = 10;
+        BufferedReader bfr = null;
+        FileWriter fw = null;
+        BufferedWriter bfw = null;
+
+        try {
+            fw = new FileWriter("cifre.txt");
+            bfw = new BufferedWriter(fw);
+            bfr = new BufferedReader(new InputStreamReader(System.in));
+            System.out.print("N: ");
+            int N = Integer.parseInt(bfr.readLine());
+            int linie[][] = new int[DIM][N];
+            String s = "";
+
+            for(int i=0; i<DIM; i++) {
+                 for(int j=0; j<N; j++) {
+                    linie[i][j] = i;
+                    s = linie[i][j] + " ";
+                    bfw.write(s);
+                    bfw.newLine();       
+                 }
+            }
+            fw.close();
+            bfw.close();
+            bfr.close();
+        } catch(IOException e) {
+            System.out.println(e);
+            System.exit(1);
+        }
+    }
+}
