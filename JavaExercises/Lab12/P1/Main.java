08d3d76bdff9a8dfe7ed5e6867b7503723ced008
Otilia Vodnitchi <otilia.vod@gmail.com>
2021-03-25 20:08:19 +0200

    Character File Example

--- JavaExercises/Lab12/P1/Main.java
+++ JavaExercises/Lab12/P1/Main.java
@@ -3,7 +3,6 @@ linie in fisierul linie.txt, cate un caracter pe fiecare linie a fisierului text
 
 package JavaExercises.Lab12.P1;
 import java.io.*;
-import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
